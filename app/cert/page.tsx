29aae280176cc204db4da74b57cd141ee20a0fd1
thivy <thivyakanth@gmail.com>
2024-11-06 08:15:35 +1100

    Add Home component with navigation link and globe icon

---
+++ app/cert/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        Go to nextjs.org →
+      </a>
+    </div>
+  );
+}
