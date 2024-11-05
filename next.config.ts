6746c352343f70cb9bbe4bce23f1d5767988896f
thivy <thivyakanth@gmail.com>
2024-11-06 08:22:16 +1100

    Update Next.js configuration for dynamic basePath and assetPrefix based on environment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: "/github-pages-demo",
+  basePath: isProd ? "/github-pages-demo" : "",
+  assetPrefix: isProd ? "/github-pages-demo/" : "",
   images: {
     domains: ["thivy.github.io"],
   },
