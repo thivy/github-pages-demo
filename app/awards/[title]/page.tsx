c79756b928db4c26794c9bd8bcfa8dec160ff5a3
thivy <thivyakanth@gmail.com>
2024-11-06 08:55:27 +1100

    Remove Cert page and add Awards page with dynamic routing and Open Graph image generation

---
+++ app/awards/[title]/page.tsx
@@ -0,0 +1,10 @@
+import { Award, awards } from "../app-data";
+
+export default async function Page({ params }: { params: Promise<Award> }) {
+  const slug = (await params).title;
+  return <div>My Post: {slug}</div>;
+}
+
+export async function generateStaticParams() {
+  return awards;
+}
---
+++ app/awards/app-data.ts
@@ -0,0 +1,58 @@
+export interface Award {
+  title: string;
+  date: string;
+  winnerName: string;
+  summary: string;
+}
+
+// test data
+export const awards: Award[] = [
+  {
+    title: "BestoftheBest",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+  {
+    title: "Best of the Best",
+    date: "2021-01-01",
+    winnerName: "John Doe",
+    summary: "This is the summary of the award.",
+  },
+];
